1113f0ce139c2af06f8cb8cbb2a912b7f84fdbf1
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-12-20 14:40:03 -0600

    Create IScalar.java

---
+++ IScalar.java
@@ -0,0 +1,29 @@
+package com.dz_fs_dev.physics;
+
+/**
+ * The user of this interface should implement a representation of a scalar.
+ * Scalars do not contain directionality.
+ *
+ * @author DZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
+public interface IScalar {
+	/**
+	 * Returns a string representation of this scalar measurement with units
+	 * represented as symbols. 
+	 * 
+	 * @return A string representation of this scalar measurement with units
+	 * represented as symbols. 
+	 */
+	public String toString();
+	
+	/**
+	 * Returns a string representation of this scalar measurement with units
+	 * explicitly spelled out.
+	 * 
+	 * @return A string representation of this scalar measurement with units
+	 * explicitly spelled out.
+	 */
+	public String toLongString();
+}
