687e5d99e3ed033c212a83817228ccca29f6573f
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-06 18:40:46 -0600

    Update SIUnitTests.java

--- tests/SIUnitTests.java
+++ tests/SIUnitTests.java
@@ -27,7 +27,7 @@ import org.junit.jupiter.api.Test;
  * 
  * @author DZ_FSDev
  * @since 17.0.1
- * @version 0.0.1
+ * @version 0.0.12
  */
 class SIUnitTests {
 
@@ -54,6 +54,19 @@ class SIUnitTests {
 				() -> assertEquals(4, new SIUnit.Meter(2).squared().getOrder(), "Meter"),
 				() -> assertEquals(4, new SIUnit.Mole(2).squared().getOrder(), "Mole"),
 				() -> assertEquals(4, new SIUnit.Second(2).squared().getOrder(), "Second")
+				);
+	}
+	
+	@Test
+	void allSIUnits_invert_negatesOrder() {
+		assertAll(new Throwable().getStackTrace()[0].getMethodName(),
+				() -> assertEquals(-2, new SIUnit.Ampere(2).inverse().getOrder(), "Ampere"),
+				() -> assertEquals(-2, new SIUnit.Ampere(2).inverse().getOrder(), "Candela"),
+				() -> assertEquals(-2, new SIUnit.Ampere(2).inverse().getOrder(), "Gram"),
+				() -> assertEquals(-2, new SIUnit.Ampere(2).inverse().getOrder(), "Kelvin"),
+				() -> assertEquals(-2, new SIUnit.Ampere(2).inverse().getOrder(), "Meter"),
+				() -> assertEquals(-2, new SIUnit.Ampere(2).inverse().getOrder(), "Mole"),
+				() -> assertEquals(-2, new SIUnit.Ampere(2).inverse().getOrder(), "Second")
 				);
 	}
 }
