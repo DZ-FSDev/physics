253c9774c40cd4af2831c1257b99d85d1914170d
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-13 16:30:26 -0600

    Add (long, long) and (long) ctors

--- Rational.java
+++ Rational.java
@@ -24,7 +24,7 @@ package com.dz_fs_dev.physics;
  * is a member of Integer.
  * 
  * @author DZ-FSDev
- * @version 0.0.4
+ * @version 0.0.5
  * @since 17.0.1
  */
 public class Rational extends Number implements Comparable<Rational>{
@@ -33,16 +33,28 @@ public class Rational extends Number implements Comparable<Rational>{
 	 */
 	private static final long serialVersionUID = 957014047269914354L;
 	
-	private final int denominator;
-	private final int numerator;
+	private final long denominator;
+	private final long numerator;
 	
 	/**
-	 * Initializes a new instance of a Rational number.
+	 * Initializes a new instance of a Rational number equivalent in value.
 	 * 
-	 * @param denominator
-	 * @param numerator
+	 * @param value The value this rational number should represent.
+	 * @since 0.0.5
 	 */
-	public Rational(int denominator, int numerator) {
+	public Rational(long value) {
+		this(value, 1);
+	}
+	
+	/**
+	 * Initializes a new instance of a Rational number given an integral
+	 * numerator and denominator.
+	 * 
+	 * @param numerator The numerator of this rational number.
+	 * @param denominator The denominator of this rational number.
+	 * @since 0.0.5
+	 */
+	public Rational(long numerator, long denominator) {
 		this.denominator = denominator;
 		this.numerator = numerator;
 	}
