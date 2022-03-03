635cf9090761b3a0f552b93f8c47863478a95057
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-03 08:59:26 -0600

    Refactor canAdd()

--- IUnit.java
+++ IUnit.java
@@ -25,8 +25,8 @@ package com.dz_fs_dev.physics;
  * @see <a href=https://physics.nist.gov/cuu/Units/checklist.html>
  *      NIST SI Unit Rules and Style Conventions</a>
  * @author DZ-FSDev
- * @since 17.0.1
- * @version 0.0.9
+ * @since 17.0.2
+ * @version 0.0.10
  */
 public interface IUnit {
 	/**
@@ -35,21 +35,21 @@ public interface IUnit {
 	 * @return The fully qualified name of the unit of measure.
 	 */
 	public String name();
-	
+
 	/**
 	 * Return the symbol that represents the unit of measure.
 	 * 
 	 * @return The symbol that represents the unit of measure.
 	 */
 	public String symbol();
-	
+
 	/**
 	 * Returns the order of this unit of measure.
 	 * 
 	 * @return The order of this unit of measure.
 	 */
 	public int getOrder();
-	
+
 	/**
 	 * Returns a new IUnit corresponding to if this IUnit was divided by
 	 * another IUnit.
@@ -58,7 +58,7 @@ public interface IUnit {
 	 * @return The resulting quotient IUnit.
 	 */
 	public IUnit divideBy(IUnit o);
-	
+
 	/**
 	 * Returns a new IUnit corresponding to if this IUnit was multiplied by
 	 * another IUnit. 
@@ -67,14 +67,16 @@ public interface IUnit {
 	 * @return The resulting product IUnit.
 	 */
 	public IUnit multiplyBy(IUnit o);
-	
-	/**	
-	 * Returns true if this IUnit can be modified in quantity by another IUnit.	
-	 * 	
-	 * @param o The other IUnit.	
-	 * @return True if this IUnit can be modified in quantity by another IUnit.	
-	 */	
-	public default boolean canAdd(IUnit o) {	
-		return o.symbol().equals(this.symbol());	
+
+	/**
+	 * Returns true if this IUnit can be modified in quantity by another IUnit.
+	 * 
+	 * @param o The other IUnit.
+	 * @return True if this IUnit can be modified in quantity by another IUnit.
+	 * @since 0.0.10
+	 */
+	public default boolean canAdd(IUnit o) {
+		return o.getClass().equals(this.getClass()) &&
+				o.getOrder() == this.getOrder();
 	}
 }
